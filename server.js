1409ca95dd9c0bbd7c16ed84bbd681dfd60b381d
Natalia Kostka <natalia7kostka@gmail.com>
2022-12-12 15:42:59 -0700

    index.jsx rendered at pokemon route

--- server.js
+++ server.js
@@ -3,14 +3,17 @@ const pokemon = require("./models/pokemon")
 const app = express()
 const port = 5500
 
+app.set('view engine', 'jsx');
+app.engine('jsx', require('express-react-views').createEngine())
 
+//GET ROUTES
 app.get('/', (req, res)=> {
     res.send('Welcome to the Pokemon App!')
 })
-
 app.get('/pokemon', (req, res) =>{
-    res.send(pokemon)
+    res.render("Index")
 })
+
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`)
